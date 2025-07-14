a2b56cb2d775e03dd18d385d812ccb57eb70c1cd
Sarthak Ahir <166507288+SarthakAhir@users.noreply.github.com>
2025-07-14 15:08:06 +0000

    Static method

---
+++ Core_Java/Two.java
@@ -0,0 +1,23 @@
+class Two{
+static{
+System.out.println("static block 1 say I am first");
+}
+static{
+System.out.println("static block 2 say I am first");
+}
+
+public static void main(String[] args){
+
+System.out.println("main() block say I am first");
+
+System.out.println("Sarthak Ahir IT");
+System.out.println("Java FS is the one of the best skills");
+}
+
+static{
+System.out.println("static block 3 say I am first");
+}
+static{
+System.out.println("static block 4 say I am first");
+}
+}
